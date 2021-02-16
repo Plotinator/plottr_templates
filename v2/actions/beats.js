445a9bba6c01f369bed5397ed9d689ce8c9865c0
Edward John Steere <edward.steere@gmail.com>
2021-02-16 14:54:18 +0200

    Squashed 'lib/pltr/' content from commit c432653
    git-subtree-dir: lib/pltr
    git-subtree-split: c4326538447e58c904cc49f5a11810efe3e28bb1

---
+++ v2/actions/beats.js
@@ -0,0 +1,28 @@
+import {
+  ADD_BEAT,
+  EDIT_BEAT_TITLE,
+  REORDER_BEATS,
+  DELETE_BEAT,
+  AUTO_SORT_BEAT,
+} from '../constants/ActionTypes'
+import { beat } from '../store/initialState'
+
+export function addBeat(bookId) {
+  return { type: ADD_BEAT, title: beat.title, bookId }
+}
+
+export function editBeatTitle(id, title) {
+  return { type: EDIT_BEAT_TITLE, id, title }
+}
+
+export function reorderBeats(beats, bookId) {
+  return { type: REORDER_BEATS, beats, bookId }
+}
+
+export function deleteBeat(id, bookId) {
+  return { type: DELETE_BEAT, id, bookId }
+}
+
+export function autoSortBeat(id) {
+  return { type: AUTO_SORT_BEAT, id }
+}
---
+++ v2/helpers/cards.js
@@ -0,0 +1,47 @@
+import { sortBy, groupBy } from 'lodash'
+
+export function sortCardsInBeat(autoSort, cards, sortedLines) {
+  if (autoSort) {
+    // group by position within the line
+    // for each position, sort those cards by the order of the lines
+    const groupedCards = groupBy(cards, 'positionWithinLine')
+    const sortedLineIds = sortedLines.map((l) => l.id)
+    return Object.keys(groupedCards).flatMap((position) => {
+      return groupedCards[position].sort(
+        (a, b) => sortedLineIds.indexOf(a.lineId) - sortedLineIds.indexOf(b.lineId)
+      )
+    })
+  } else {
+    return sortBy(cards, 'positionInBeat')
+  }
+}
+
+export function cardMapping(sortedBeats, sortedLines, card2Dmap, currentLine) {
+  return sortedBeats.reduce((acc, beat) => {
+    acc[beat.id] = sortedBeatCards(sortedLines, beat.id, card2Dmap, currentLine)
+    return acc
+  }, {})
+}
+
+function lineIsHidden(line, currentLine) {
+  if (!currentLine) return false
+  return line.id != currentLine
+}
+
+function sortedBeatCards(sortedLines, beatId, card2Dmap, currentLine) {
+  return sortedLines.reduce((acc, l) => {
+    if (lineIsHidden(l, currentLine)) return acc
+
+    const cards = card2Dmap[`${l.id}-${beatId}`]
+    if (cards) {
+      return acc.concat(cards)
+    } else {
+      return acc
+    }
+  }, [])
+}
+
+export function truncateTitle(title, maxLength) {
+  if (title.length < maxLength) return title
+  return title.substr(0, maxLength) + '...'
+}
